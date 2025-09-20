30f76aa37814dba1455a35a665332caa352f47d5
Rafael Freitas <rafaelevoinfo@gmail.com>
2025-09-20 00:07:01 -0300

    Tentando fazer deploy no Render

--- firebase.js
+++ firebase.js
@@ -29,7 +29,8 @@ if (process.env.DESENVOLVIMENTO) {
   });
 } else {
   firebase_admin.initializeApp({
-    credential: firebase_admin.credential.applicationDefault()
+    //credential: firebase_admin.credential.applicationDefault()
+    credential: firebase_admin.credential.Certificate("etc/secrets/serviceAccountKey.json")
   });
 }
 
